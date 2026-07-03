189e882479e9d0f0db26c651b87a4d16eea00510
Yashtagad12 <tagadyash12@gmail.com>
2026-07-03 23:01:26 +0530

    Added a dynamic calender

--- src/Pages/Calender.jsx
+++ src/Pages/Calender.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useMemo } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
 
-const Calender = () => {
+import { Header } from '../Components';
+import { scheduleData } from '../data/dummy';
+
+const Calendar = () => {
+
+  // Convert Syncfusion data to FullCalendar format
+  const events = useMemo(() => {
+    return scheduleData.map((event) => ({
+      id: event.Id,
+      title: event.Subject,
+      start: event.StartTime,
+      end: event.EndTime,
+      allDay: event.IsAllDay,
+      backgroundColor: event.CategoryColor,
+      borderColor: event.CategoryColor,
+    }));
+  }, []);
+
   return (
-    <div>Calender</div>
-  )
-}
+    <div className="m-2 md:m-10 p-5 md:p-8 bg-white dark:bg-secondary-dark-bg rounded-xl shadow">
 
-export default Calender
+      <Header category="App" title="Calendar" />
+
+      <div className="mt-8">
+
+        <FullCalendar
+          plugins={[
+            dayGridPlugin,
+            timeGridPlugin,
+            interactionPlugin,
+            listPlugin,
+          ]}
+
+          initialView="dayGridMonth"
+
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+          }}
+
+          events={events}
+
+          editable={true}
+
+          selectable={true}
+
+          dayMaxEvents={true}
+
+          height="80vh"
+
+          eventClick={(info) => {
+            alert(
+              `Event : ${info.event.title}\n\nStart : ${info.event.start}`
+            );
+          }}
+
+          select={(info) => {
+            alert(
+              `Selected\n${info.startStr}\n\n${info.endStr}`
+            );
+          }}
+
+        />
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Calendar;

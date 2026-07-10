d48360d06772c74ae9d9b1e53103e82360742539
Yashtagad12 <tagadyash12@gmail.com>
2026-07-10 11:43:57 +0530

    Added line chart

--- src/Pages/charts/Line.jsx
+++ src/Pages/charts/Line.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../../Components";
+import LineChartComponent from "../../Components/Charts/LineChart";
 
 const Line = () => {
   return (
-    <div>Line</div>
-  )
-}
+    <div className="m-2 md:m-10 p-5 md:p-8 bg-white dark:bg-secondary-dark-bg rounded-2xl shadow">
 
-export default Line
+      <Header
+        category="Chart"
+        title="Line Chart"
+      />
+
+      <div className="mt-8">
+        <LineChartComponent />
+      </div>
+
+    </div>
+  );
+};
+
+export default Line;
